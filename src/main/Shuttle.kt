da63016953e9576d2de0e132401b25d89febbcde
David Geretti <david.geretti@bestmile.com>
2018-12-18 22:08:43 +0100

    Make shuttle turn left and right

--- src/main/Shuttle.kt
+++ src/main/Shuttle.kt
@@ -4,8 +4,23 @@ class Shuttle(
     var bearing: String = "N"
         private set
 
-    fun execute(mission: String) {
-        bearing = "E"
-    }
+    private val right = mapOf(
+        "E" to "S",
+        "S" to "W",
+        "W" to "N",
+        "N" to "E"
+    )
 
+    private val left = mapOf(
+        "E" to "N",
+        "S" to "E",
+        "N" to "W",
+        "W" to "S"
+    )
+
+    fun execute(mission: String) = when(mission) {
+        "R" -> bearing = right[bearing]!!
+        "L" -> bearing = left[bearing]!!
+        else -> throw Error("Invalid mission $mission")
+    }
 }
--- src/test/ShuttleShould.kt
+++ src/test/ShuttleShould.kt
@@ -1,20 +1,57 @@
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class ShuttleShould {
-    @Test
-    fun `report an initial position and bearing`() {
-        val shuttle = Shuttle()
-        assertEquals(0, shuttle.position.X)
-        assertEquals(0, shuttle.position.Y)
-        assertEquals("N", shuttle.bearing)
+
+    lateinit var shuttle: Shuttle
+
+    val RIGHT_TURN = "R"
+    val LEFT_TURN= "L"
+    val NORTH = "N"
+    val EAST = "E"
+    val WEST = "W"
+    val SOUTH= "S"
+
+    @Before
+    fun initialise() {
+        shuttle = Shuttle()
     }
 
     @Test
-    fun `right turns updates bearing`() {
-        val shuttle = Shuttle()
+    fun `report an initial position and bearing`() {
+        assertEquals(0, shuttle.position.X)
+        assertEquals(0, shuttle.position.Y)
+        assertEquals(NORTH, shuttle.bearing)
+    }
 
-        shuttle.execute("R")
-        assertEquals("E", shuttle.bearing)
+    @Test
+    fun `right turns makes the shuttle go through all directions`() {
+        shuttle.execute(RIGHT_TURN)
+        assertEquals(EAST, shuttle.bearing)
+
+        shuttle.execute(RIGHT_TURN)
+        assertEquals(SOUTH, shuttle.bearing)
+
+        shuttle.execute(RIGHT_TURN)
+        assertEquals(WEST, shuttle.bearing)
+
+        shuttle.execute(RIGHT_TURN)
+        assertEquals(NORTH, shuttle.bearing)
+    }
+
+    @Test
+    fun `left turns makes the shuttle go through all directions`() {
+        shuttle.execute(LEFT_TURN)
+        assertEquals(WEST, shuttle.bearing)
+
+        shuttle.execute(LEFT_TURN)
+        assertEquals(SOUTH, shuttle.bearing)
+
+        shuttle.execute(LEFT_TURN)
+        assertEquals(EAST, shuttle.bearing)
+
+        shuttle.execute(LEFT_TURN)
+        assertEquals(NORTH, shuttle.bearing)
     }
 }
